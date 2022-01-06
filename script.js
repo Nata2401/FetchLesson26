63578914378fc1306750dfd14347c78e34a09706
Nata2401 <kuzyan.rd@yandex.ru>
2022-01-06 22:50:31 +0300

    Выполнено задание 26

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const getData = ({ url, method = "GET" }) => fetch(url, { method });
 
+const sendData = ({ url, method = "POST", data = {} }) =>
+  fetch(url, { method, body: data });
+
+getData({ url: "db.json" })
+  .then((data) => data.json())
+  .then((json) => {
+    console.log(json);
+
+    sendData({
+      url: "https://jsonplaceholder.typicode.com/posts",
+      data: JSON.stringify(json),
+    })
+      .then(() => {
+        console.log("Данные успешно отправлены");
+      })
+      .catch(() => console.error(new Error("Ошибка отправки данных")));
+  })
+  .catch(() => {
+    console.error(new Error(`Ошибка при получении файла`));
+  });
